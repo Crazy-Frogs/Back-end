7793b9408a669dbf3d7de35c747c816aabab8f55
João Emanuel Pessoa <joao.emanuel.pn@gmail.com>
2025-08-22 21:29:12 -0300

    ✨ feat: Cria o serviço genérico de envio de e-mail

--- src/main/java/sesi/petvita/PetVitaApplication.java
+++ src/main/java/sesi/petvita/PetVitaApplication.java
@@ -2,10 +2,12 @@ package sesi.petvita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class PetVitaApplication {
 
     public static void main(String[] args) {
--- src/main/java/sesi/petvita/notification/service/EmailService.java
+++ src/main/java/sesi/petvita/notification/service/EmailService.java
@@ -1,18 +1,42 @@
 package sesi.petvita.notification.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
+    @Autowired
+    private JavaMailSender mailSender;
 
-    //Teste ainda irei por o springmail.
+    // Pega o e-mail remetente do application.properties
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    /**
+     * Envia um e-mail de forma assíncrona.
+     * A anotação @Async faz com que este método rode em uma thread separada,
+     * não bloqueando a execução principal da aplicação.
+     */
+    @Async
     public void sendEmail(String to, String subject, String body) {
-        System.out.println("========================================================");
-        System.out.println("SIMULANDO ENVIO DE E-MAIL");
-        System.out.println("PARA: " + to);
-        System.out.println("ASSUNTO: " + subject);
-        System.out.println("CORPO: " + body);
-        System.out.println("========================================================");
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(fromEmail);
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+
+            mailSender.send(message);
+            System.out.println("E-mail de lembrete enviado com sucesso para: " + to);
+        } catch (MailException e) {
+            // Em um projeto real, você usaria um logger (SLF4J)
+            System.err.println("Erro ao enviar e-mail para " + to + ": " + e.getMessage());
+        }
     }
 }
